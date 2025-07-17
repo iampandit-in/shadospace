35589a2557a64f9b63f5116873231f5842acf08d
Pandit <iampandit.in@gmail.com>
2025-07-17 10:39:05 +0530

    Refactor Feed component to improve code readability by standardizing formatting and indentation in the JSX structure

--- components/feed.tsx
+++ components/feed.tsx
@@ -26,39 +26,39 @@ export default async function Feed() {
       {data.map((a) => (
         <div key={a.posts.id} className="mt-4 grid-cols-2">
           <Link href={`/post/${a.posts.id}`}>
-          <Card>
-            <CardHeader>
-              <CardTitle>{a.posts.title}</CardTitle>
-              <CardDescription>{a.posts.content}</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <Image
-                src={a.posts.image || ""}
-                alt={a.posts.title}
-                width={1000}
-                quality={100}
-                className="rounded-md w-full"
-                height={1000}
-              />
-            </CardContent>
-            <CardFooter>
-              <CardDescription className="flex items-center gap-2">
-                <Avatar>
-                  <AvatarImage
-                    className="object-cover"
-                    src={a.users.image || ""}
-                    alt="@shadcn"
-                  />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <span>
-                  <p>{a.users.name}</p>
-                  <p>{a.users.createdAt.toLocaleDateString()}</p>
-                </span>
-              </CardDescription>
-            </CardFooter>
-          </Card>
-        </Link>
+            <Card>
+              <CardHeader>
+                <CardTitle>{a.posts.title}</CardTitle>
+                <CardDescription>{a.posts.content}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Image
+                  src={a.posts.image || ""}
+                  alt={a.posts.title}
+                  width={1000}
+                  quality={100}
+                  className="rounded-md w-full"
+                  height={1000}
+                />
+              </CardContent>
+              <CardFooter>
+                <CardDescription className="flex items-center gap-2">
+                  <Avatar>
+                    <AvatarImage
+                      className="object-cover"
+                      src={a.users.image || ""}
+                      alt="@shadcn"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                  <span>
+                    <p>{a.users.name}</p>
+                    <p>{a.users.createdAt.toLocaleDateString()}</p>
+                  </span>
+                </CardDescription>
+              </CardFooter>
+            </Card>
+          </Link>
         </div>
       ))}
     </div>
