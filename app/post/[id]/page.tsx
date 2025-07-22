c806a95199085d6efea21529de63345a83fe6258
Pandit Pawar <iampandit.in@gmail.com>
2025-07-22 22:10:13 +0530

    Refactor database queries in Post component to use updated schema imports for improved clarity

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import db from "@/db";
-import { postsTable, usersTable } from "@/db/schema";
+import { post, user } from "@/db/schema";
 import { desc, eq } from "drizzle-orm";
 import Image from "next/image";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
@@ -8,7 +8,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default async function Post({ params }: { params: Promise<{ id: string }> }) {
   const {id} = await params
-  const data = await db.select().from(postsTable).where(eq(postsTable.id, id)).innerJoin(usersTable, eq(postsTable.userId, usersTable.id));
+  const data = await db.select().from(post).where(eq(post.id, id)).innerJoin(user, eq(post.userId, user.id));
 
   return (
     <div className="max-w-7xl mx-auto p-5 mt-20 grid col-span-1 md:grid-cols-2 lg:grid-cols-3 3xl:grid-cols-4 gap-4">
